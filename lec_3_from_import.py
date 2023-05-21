6cd81534e699332a3d8a9755874e07f5c091f2c8
Jesterus <127763879+Jesterus@users.noreply.github.com>
2023-05-21 19:35:02 +0000

    less_4

--- lec_3_from_import.py
+++
@@ -1,9 +0,0 @@
-#Копирование указанных атрибутов модуля
-from lec_3_my_module import a, b
-
-print(a * b)
-
-# Копирование ВСЕХ атрибутов модуля
-from lec_3_my_module import *
-
-print(c[2] * c[1])
--- lec_3_import_as.py
+++
@@ -1,9 +0,0 @@
-# Создание псевдонима для имени модуля
-import lec_3_my_module as mm
-
-print(mm.a)
-
-b = mm.b * 3
-print(b)
-
-print(mm.c[2] + b + mm.c[0])
